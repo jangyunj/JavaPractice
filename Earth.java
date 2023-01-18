a2a148ea4e292974940f2254d8c90f6f8be80a33
Yun Jang <jang.yunj7@gmail.com>
2023-01-17 23:20:48 -0500

    initial commit

---
+++ Earth.java
@@ -0,0 +1,14 @@
+
+public class Earth {
+	public static void main(String args[]) {
+		Human tom;
+		tom = new Human();
+		
+		tom.age = 5;
+		tom.eyeColor = "brown";
+		tom.heightInches = 72;
+		tom.name = "Tom Sawyer";
+		
+		tom.speak();
+	}
+}
---
+++ module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author hwait
+ *
+ */
+module Prerequisite {
+}
